97d005ccb649c6abbc18def21fc6cdf9a5731ef0
1raviprakash <42237336+1raviprakash@users.noreply.github.com>
2020-08-10 01:20:23 +0530

    Create QUESTION2.cpp

---
+++ QUESTION2.cpp
@@ -0,0 +1,18 @@
+//library
+#include<iostream>
+
+using namespace std;
+//write the main function
+int main()
+{
+    // declaring variable
+    double result = 1;
+    //using for loop
+    for(int i=1;i<=15;i++)
+    {
+        result *= i; 
+    
+    }
+    cout << result << endl;
+
+}
