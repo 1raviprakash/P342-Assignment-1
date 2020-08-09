e538558965c0700c7f1ce27af76328dd945ec36a
1raviprakash <42237336+1raviprakash@users.noreply.github.com>
2020-08-10 01:17:14 +0530

    Create QUESTION1.cpp

---
+++ QUESTION1.cpp
@@ -0,0 +1,16 @@
+//library
+#include<iostream>
+
+using namespace std;
+//write the main function
+int main()
+{
+    // declaring variable
+    int result = 0;
+    for(int a=0;a<=100;a++)
+    {
+        result += i; 
+    
+    }
+    cout << result << endl ;
+}
